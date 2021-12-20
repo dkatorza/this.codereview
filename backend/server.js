98969c3b1038310f1ddf24e2a2a7b7b867bc276e
dkatorza <dkatorza@gmail.com>
2021-12-20 20:49:40 +0200

    setting folders structure

---
+++ backend/server.js
@@ -0,0 +1,73 @@
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const cors = require('cors')
+const path = require('path')
+const expressSession = require('express-session')
+const app = express()
+const http = require('http').createServer(app)
+
+
+const session = expressSession({
+    secret: '1985dk1709%$!^@shi',
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false }
+})
+
+app.use(session)
+
+// Config Express Application
+app.use(express.json())
+app.use(cookieParser())
+app.use(session)
+app.use(express.static('public'))
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.resolve(__dirname, 'public')))
+    app.use((req, res, next) => {
+        if (req.header('x-forwarded-proto') !== 'https')
+            res.redirect(`https://${req.header('host')}${req.url}`)
+        else
+            next()
+    })
+} else {
+    const corsOptions = {
+        origin: ['http://127.0.0.1:8080', 'http://localhost:8080', 'http://127.0.0.1:3000', 'http://localhost:3000'],
+        credentials: true
+    }
+    app.use(cors(corsOptions))
+}
+
+// ROUTES REQ
+const authRoutes = require('./api/auth/auth.routes')
+const userRoutes = require('./api/user/user.routes')
+const boardRoutes = require('./api/board/project.routes')
+
+
+
+// routes
+const setupAsyncLocalStorage = require('./middlewares/setupAls.middleware')
+app.all('*', setupAsyncLocalStorage)
+
+app.get('/api/setup-session', (req, res) => {
+    req.session.connectedAt = Date.now()
+    console.log('setup-session:', req.sessionID);
+    res.end()
+})
+
+// USING ROUTES
+app.use('/api/auth', authRoutes)
+app.use('/api/user', userRoutes)
+app.use('/api/project', boardRoutes)
+
+
+app.get('/**', (req, res) => {
+    res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+const logger = require('./services/logger.service')
+const port = process.env.PORT || 3030
+http.listen(port, () => {
+    logger.info('Server is running on port: ' + port)
+})
